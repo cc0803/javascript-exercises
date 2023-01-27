0cb535680682f9736aed464f918f9408a6eb53c0
cc0803 <simongeldbach@gmail.com>
2023-01-27 12:51:16 +0100

    Solve Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let split = string.split("");
+    let reversed = [];
+    let result = "";
+    
+    for (let i = 0; i < string.length; i++){
+        reversed.push(split.pop());
+    }
 
+    result = reversed.join("");
+    return result;
 };
 
 // Do not edit below this line
