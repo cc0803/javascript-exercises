5248892f640ad891585a400bf2ae5fb7c403ac77
cc0803 <simongeldbach@gmail.com>
2023-01-27 12:37:58 +0100

    solve exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let resultString = "";
+    if (number < 0){
+        return "ERROR";
+    }
+    
+    for (let i = 0; i < number; i++){
+        resultString += string;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
