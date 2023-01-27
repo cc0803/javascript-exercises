71712f915e5a7c14544460c2a8342df450b61f67
cc0803 <simongeldbach@gmail.com>
2023-01-27 17:23:25 +0100

    solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    
+    let sum = 0;
+    if (typeof(num1) !== typeof(1) || typeof(num2) != typeof(1)){
+        return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0 ){
+        return "ERROR";
+    }
+    // Checking to see wich number is smaller so i is initialized to that one
+    if (num1 < num2){
+        for (let i = num1; i <= num2; i ++){
+            sum += i;
+        }
+    } else {
+        for (let i = num2; i <= num1; i ++){
+            sum += i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
