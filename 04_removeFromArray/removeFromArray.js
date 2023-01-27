28ea05ffa37bf3d2f9ce5cabc8e7285618eed98b
cc0803 <simongeldbach@gmail.com>
2023-01-27 17:11:43 +0100

    solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...otherargs) {
 
+    for (let i = 0; i < otherargs.length; i++){
+        for (let j = 0; j < array.length; j++){
+            if(array[j] === otherargs[i]){
+                array.splice(j, 1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
