e5697d04baccd06a28cf4de30ee8edcd71929773
cc0803 <simongeldbach@gmail.com>
2023-02-08 15:41:58 +0100

    Solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array, total = 0) {
+	array.map(element => total += element);
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array, total = 1) {
+  array.map(item => total *= item);
+  return total;
+} ;
 
+const power = function(a, b) {
+	return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0){
+    return 1;
+  }
+  let total = 1;
+	for (let i = 1; i <= a; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
