18c667447bc43e8cd9e569bbeaf52e583029fc51
cc0803 <simongeldbach@gmail.com>
2023-02-10 16:24:49 +0100

    solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (Number(number) == NaN || Number(number) < 1){
+        return "OOPS";
+    } 
+    number = Number(number);
 
+
+    let sequence = [1, 1];
+
+    // number - 2 because the sequence already has two elements
+    for (let i = 0; i < (number - 2); i++){
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    }
+    return sequence[sequence.length - 1];
 };
 
 // Do not edit below this line
