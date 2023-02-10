dc7a2c4a897d5aa699aca3da23f747b0c8b28f95
cc0803 <simongeldbach@gmail.com>
2023-02-10 15:54:10 +0100

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let proccessed = string.toLowerCase().replace(/[^a-z]/g, "");
+    let array = proccessed.split("");
 
-};
+    for(let i = 0; i < proccessed.length; i++){
+        if (array[i] != array[proccessed.length - i - 1]){
+            return false;
+        }
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// String zu array machen
+// Aus Array alle Kommas, Ausrufezeichen, Punkte, Fragezeichen und Leerzeichen entfernen
+// Element für element von hinten nach vorne vergreichen
