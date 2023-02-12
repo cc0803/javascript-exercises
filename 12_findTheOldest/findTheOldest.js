63f8f57e0ea605730c709fe2b5c52c05ee00a533
cc0803 <simongeldbach@gmail.com>
2023-02-12 09:32:04 +0100

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+let currentYear = new Date().getFullYear();
 
-};
+const findTheOldest = function(people) {
+    let oldestPerson = people.reduce((oldest, person) => {
+        if (oldest.yearOfBirth === undefined){
+            return person;
+        }
+        if (checkForAge(oldest) > checkForAge(person)){
+            return oldest;
+        } else {
+            return person;
+        }
+    });
+
+    return oldestPerson;
+}
+
+function checkForAge(person) {
+    if (person.yearOfDeath === undefined){
+        return currentYear - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
